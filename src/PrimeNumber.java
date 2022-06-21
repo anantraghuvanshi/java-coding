ad16fc5641fb17841e132d8240bd5026cf5a7194
Anant <anantsinghraghuvanshi1@gmail.com>
2022-06-21 16:54:33 +0530

    code done

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -8,7 +8,7 @@ public class PrimeNumber{
         System.out.println(isPrime(n));
     }
     static boolean isPrime(int n){
-        for (int i = 2; i <Math.sqrt(n) ; i++) {
+        for (int i = 2; i <=Math.sqrt(n) ; i++) {
             if(n % i ==0){
                 return false;
             }
