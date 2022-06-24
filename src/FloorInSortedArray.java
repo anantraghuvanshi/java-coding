4d215209de54a44f3671dcbfe6eb26a4f2d6b0a5
Anant <anantsinghraghuvanshi1@gmail.com>
2022-06-24 16:31:43 +0530

    floor update

--- src/FloorInSortedArray.java
+++ src/FloorInSortedArray.java
@@ -23,6 +23,9 @@ public class FloorInSortedArray {
         int start =0;
         int end = arr.length-1;
 
+        if(target < arr[arr.length-1]){
+            return -1;
+        }
 
         while(start <= end){
             int mid = start + (end-start) / 2;
