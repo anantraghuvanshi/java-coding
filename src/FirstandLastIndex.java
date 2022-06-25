1c6d59ea1aa3a2891d839d46a15064549d850c2f
Anant <anantsinghraghuvanshi1@gmail.com>
2022-06-25 12:34:47 +0530

    FirstandLastIndex update

--- src/FirstandLastIndex.java
+++ src/FirstandLastIndex.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 public class FirstandLastIndex {
 
     //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
     public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+
+        System.out.println(Arrays.toString(searchRange(nums, target)));
 
     }
-    public int[] searchRange(int[] nums, int target) {
+    public static int[] searchRange(int[] nums, int target) {
 
         int[] ans = {-1, -1};
 
@@ -17,7 +23,7 @@ public class FirstandLastIndex {
 
         return ans;
     }
-    int search(int[] nums, int target, boolean findStartIndex){
+    public static int search(int[] nums, int target, boolean findStartIndex){
         int ans =-1;
         int start =0;
         int end = nums.length-1;
