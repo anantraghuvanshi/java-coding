79d4c6a68c6c38522b5f9235450a829e4cb60612
Anant <anantsinghraghuvanshi1@gmail.com>
2022-06-27 13:25:03 +0530

    code done

--- src/SearchInRotatedSortedArray.java
+++ src/SearchInRotatedSortedArray.java
@@ -1,5 +1,8 @@
 public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr, target));
 
     }
     static int findPivot(int[] arr){
@@ -22,7 +25,7 @@ public class SearchInRotatedSortedArray {
         }
         return -1;
     }
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         int pivot = findPivot(nums);
 
         if (pivot == -1){
