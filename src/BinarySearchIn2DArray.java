687b9684e650238137fd7ba593a51a12b3559717
Anant <anantsinghraghuvanshi1@gmail.com>
2022-07-04 13:39:56 +0530

    code done

---
+++ src/BinarySearchIn2DArray.java
@@ -0,0 +1,22 @@
+public class BinarySearchIn2DArray {
+    public static void main(String[] args) {
+
+        //not in mood to write the main function.
+
+    }
+    static int[] search(int[][] arr, int target){
+        int row = 0;
+        int column = arr.length -1;
+
+        while(row < arr.length && c >=0){
+            if(arr[row][column] == target){
+                return new int[]{row,column};
+            }
+            if(arr[row][column] < target){
+                row++;
+            }
+            else column--;
+        }
+        return new int[]{-1, -1};
+    }
+}
